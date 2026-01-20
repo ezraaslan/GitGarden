ba2b621a58c0a4168eba352a5d8ebf975c074d90
ezraaslan <ezraaslan10@gmail.com>
2026-01-20 16:40:15 -0500

    flower stem

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
 import subprocess
+import random
+import time
+import os
+
+
+def flower(x, y, commits, canvas):
+    for i, commit in enumerate(commits,):
+    # grow upward one cell per commit
+        if y <= 0:
+            break
+
+        canvas[y][x] = "|"
+        canvas[y][x+1] = "|"
+        y -= 1
+
+        if i % 4 == 0:
+            x += random.randint(-1,1)
+
+        if i % random.randint(5, 8):
+            canvas[y-3][x+1] = "/"
+        elif i % random.randint(3, 6):
+            canvas[y-1][x-1] = "\\"
+
+        clear()
+        for row in canvas:
+            print("".join(row))
+        time.sleep(0.05)
+
+
 
 def get_git_commits():
     result = subprocess.run(
@@ -15,4 +44,26 @@ def get_git_commits():
             "refs": refs
         })
     return commits
-print(get_git_commits())
+
+commits = get_git_commits()
+commits.reverse()
+
+WIDTH = 60
+HEIGHT = 25
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+canvas = [[" "] * WIDTH for _ in range(HEIGHT)]
+
+x = WIDTH // 2
+y = HEIGHT - 1
+
+# 1 = seed
+
+# 2-3 = stem
+
+# 4-7 = flower
+flower(x, y, commits, canvas)
+
+# 8 or more = tree
